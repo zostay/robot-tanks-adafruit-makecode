0527244b7b03737442d29b46cc59038f87a9ad0f
Sterling Hanenkamp <sterling@hanenkamp.com>
2019-07-21 22:28:25 -0500

    try again

--- tank.ts
+++ tank.ts
@@ -3,7 +3,7 @@ namespace Tank {
      * Move the tank forward.
      */
     //% blockId=tank_move_forward
-    //% block="forward|%distance=0|cm"
+    //% block="forward|%distance|cm"
     //% distance.min=0 distance.max=100 x.defl=1
     export function moveForward(distance: Number): void {
 
@@ -13,7 +13,7 @@ namespace Tank {
      * Turn the tank left.
      */
     //% blockId=tank_turn_left
-    //% block="turn left|%degrees=90|degrees"
+    //% block="turn left|%degrees|degrees"
     //% degrees.min=0 degrees.max=360 x.defl=90
     export function turnLeft(degrees: Number): void {
 
@@ -23,7 +23,7 @@ namespace Tank {
      * Turn the tank right.
      */
     //% blockId=tank_turn_right
-    //% block="turn right|%degrees=90|degrees"
+    //% block="turn right|%degrees|degrees"
     //% degrees.min=0 degrees.max=360 x.defl=90
     export function turnRight(degrees: Number): void {
 
