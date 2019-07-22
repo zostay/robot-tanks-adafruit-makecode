21a63187f0bc2db46be3e4f314b6b40d82e40dc5
Sterling Hanenkamp <sterling@hanenkamp.com>
2019-07-21 22:17:27 -0500

    Adding stubs for forward, left, and right turns

---
+++ tank.ts
@@ -0,0 +1,28 @@
+namespace Tank {
+    /**
+     * Move the tank forward.
+     */
+    //% blockId=tank_move_forward
+    //% block="forward|number %d|cm"
+    export function moveForward(d: Number): void {
+
+    }
+
+    /**
+     * Turn the tank left.
+     */
+    //% blockId=tank_turn_left
+    //% block="turn left|number %deg|degrees"
+    export function turnLeft(deg: Number): void {
+
+    }
+
+    /**
+     * Turn the tank right.
+     */
+    //% blockId=tank_turn_right
+    //% block="turn right|number %deg|degrees"
+    export function turnRight(deg: Number): void {
+        
+    }
+}
